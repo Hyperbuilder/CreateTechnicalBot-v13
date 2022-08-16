96f71ddf33a1ea6ec885e0c2f0c9eb7ff9551d32
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2022-08-16 14:14:19 +0200

    Hotfix #1

--- Events/Member/guildMemberAdd.js
+++ Events/Member/guildMemberAdd.js
@@ -11,6 +11,10 @@ module.exports = {
 
         // add role //
 
+        await guild.roles.fetch() //optional - put it if the role is valid, but is not cached
+        let role = guild.roles.cache.find(role => role.id === '827226948102127656')
+        member.roles.add(role)
+
         const Welcomer = new WebhookClient({ id: "926506667816939560", token: `${client.config.WelcomeToken}` })
         const Logger = new WebhookClient({ id: "926506667816939560", token: `${client.config.LogToken}` })
 
@@ -20,6 +24,7 @@ module.exports = {
             .setDescription(`Welcome ${member} to **${guild.name}**\nAccount Created: <t:${parseInt(user.createdTimestamp / 1000)}:R>\nMember Count: **${guild.memberCount}**`)
             .setFooter({ text: `ID: ${user.id}` })
 
+        Welcomer.send({ content: `${member}` })
         Welcomer.send({ embeds: [Welcome] })
     }
 }
