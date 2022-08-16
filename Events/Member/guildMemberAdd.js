48416c69362d145cafdfa10c4466fa67b2065f52
Hyperbuilder <67744001+Hyperbuilder@users.noreply.github.com>
2022-08-16 14:34:07 +0200

    Hotfix #2.2

--- Events/Member/guildMemberAdd.js
+++ Events/Member/guildMemberAdd.js
@@ -15,14 +15,14 @@ module.exports = {
         let role = guild.roles.cache.find(role => role.id === '827226948102127656')
         member.roles.add(role)
 
-        const Welcomer = new WebhookClient({ id: "926506667816939560", token: `${client.config.WelcomeToken}` })
+        const Welcomer = new WebhookClient({ id: "1009064379033059428", token: `${client.config.WelcomeToken}` })
         const Logger = new WebhookClient({ id: "926506667816939560", token: `${client.config.LogToken}` })
 
         const Welcome = new EmbedBuilder()
             .setColor("#000080")
             .setAuthor({ name: user.tag, iconUrl: user.avatarURL({ dynamic: true, size: 512 }) })
             .setDescription(`Welcome ${member} to **${guild.name}**\nStart you application form in <#1006613986277589062>\nYou can also checkout our Public offerings! <#949407560014917693>\nMember Count: **${guild.memberCount}**`)
-            .setFooter({ text: `ID: ${user.id}` })
+            .setFooter({ text: `${user.username} Joined ${guild.name}` })
 
         Welcomer.send({ content: `${member}` })
         Welcomer.send({ embeds: [Welcome] })
